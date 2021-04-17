04c466422623f93d6aafdbb4b31662361155fa31
Manoj-Bittu <knmanoj2@gmail.com>
2021-04-17 11:44:31 +0530

    added

--- src/shopping/Home.java
+++ src/shopping/Home.java
@@ -1,10 +1,19 @@
 package shopping;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Home {
 
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		Thread.sleep(2000);
+		driver.close();
 	}
 
 }
